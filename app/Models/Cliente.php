79d8e5b8431588529ab61ef6ee1dec751ebe34f3
Acerola-BP11 <felipegrego23@outlook.com>
2023-05-18 19:28:56 -0300

    New Models

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    use HasFactory;
 
     protected $fillable = ['nome', 'rua', 'complemento', 'cidade', 'numero_fixo', 'ddd_fixo', 'numero_cel', 'ddd_cel',
 'cpf', 'nascimento', 'rg'];
