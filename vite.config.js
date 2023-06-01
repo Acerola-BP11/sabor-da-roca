7459099504f0cdabf003cfca62d5642faed9c5fc
Acerola-BP11 <felipegrego23@outlook.com>
2023-06-01 00:10:07 -0300

    React

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**',
-            ],
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
+            refresh: true,
         }),
+        react(),
     ],
 });
