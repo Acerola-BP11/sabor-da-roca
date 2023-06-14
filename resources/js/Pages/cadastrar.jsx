26ffb46ed6fe8f92c4b86b91e718a11adf5c638b
VitorMartins47 <vitorhavila83@gmail.com>
2023-06-13 21:59:37 -0300

    axios e os kralho

--- resources/js/Pages/cadastrar.jsx
+++ resources/js/Pages/cadastrar.jsx
@@ -1,23 +1,32 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import TextInput from '@/components/TextInput';
+import PrimaryButton from '@/components/PrimaryButton';
+
+const axios = require('axios')
 
 export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Area Administrativa</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Cadastrar Marmita</h2>}
 >
             <Head title="cadastrar" />
 
             <div>
         <TextInput type = "text" placeholder = "Preço"/>
-        <TextInput placeholder = "Tamanho"/>
-        <TextInput placeholder = "Tara"/>
-        <TextInput placeholder = "Detalhamento"/>
-
+            </div>
+            <div>
+        <TextInput type = "text" placeholder = "Tamanho"/>
+            </div>
+            <div>
+        <TextInput type = "text" placeholder = "Tara"/>
+            </div>
+            <div>
+        <TextInput type = "text" placeholder = "Detalhamento"/>
             </div>
             
+            <PrimaryButton className='submit'> Enviar </PrimaryButton>
         </AuthenticatedLayout>
     );
 }
