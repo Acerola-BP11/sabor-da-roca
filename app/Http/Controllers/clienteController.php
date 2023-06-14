8217a46c051e8a46b2730ddc5cbfbe0ee8afaa69
Acerola-BP11 <felipegrego23@outlook.com>
2023-06-14 19:21:03 -0300

    patch: delete cliente

--- app/Http/Controllers/clienteController.php
+++ app/Http/Controllers/clienteController.php
@@ -59,7 +59,7 @@ class ClienteController extends Controller
         return redirect()->route('listclient');
     }
 
-    public function delete(Request $id)
+    public function delete(Request $data, $id)
     {
         $cliente = Cliente::find($id);
         $cliente->delete();
