7c9b7cdb79f14d492796268286d868f6abcdfee8
Acerola-BP11 <felipegrego23@outlook.com>
2023-06-14 12:12:30 -0300

    if else

--- app/Http/Controllers/clienteController.php
+++ app/Http/Controllers/clienteController.php
@@ -9,7 +9,11 @@ class clienteController extends Controller
 {
     public function list(){
         $clientes = Cliente::all();
-        return view('lista_clientes', [$clientes]);
+        if ($clientes.count <= 0){
+            return view('lista_clientes', ['Não há nenhum item cadastrado!']);
+        }else{
+            return view('lista_clientes', [$clientes]);
+        }
     }
     public function new(){
         return view('new_cliente');
@@ -31,9 +35,26 @@ class clienteController extends Controller
         $cliente->save();
         return redirect('/listClients');
     }
-    
+
+    public function edit($id){
+        $cliente = Cliente::find($id);
+        $cliente->nome = $dados->input("nome");
+        $cliente->rua = $dados->input("rua");
+        $cliente->complemento = $dados->input("complemento");
+        $cliente->cidade = $dados->input("cidade");
+        $cliente->numero_fixo = $dados->input("numero_fixo");
+        $cliente->ddd_fixo = $dados->input("ddd_fixo");
+        $cliente->ddd_cel = $dados->input("ddd_cel");
+        $cliente->numero_cel = $dados->input("numero_cel");
+        $cliente->cpf = $dados->input("cpf");
+        $cliente->nascimento = $dados->input("nascimento");
+        $cliente->rg = $dados->input("rg");
+        $cliente->save();
+        return redirect('/listClients');
+    }
+
     public function excluir($id){
         $cliente = Cliente::find($id);
-        $cliente->delete();
+        $cliente->destroy();
     }
 }
